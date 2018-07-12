be52f91ff5a1c83d46ab5f875e195004416017b7
neilo6000 <neilo6000@gmail.com>
2018-07-12 00:12:22 -0600

    Version 2

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def submit_credentials():
     if len(username)  < 4 or len(username) > 20:
         username_error = "That's not a valid username"
         
-    # Process Username.
+    # Process Password.
     password=request.form['password'] 
     password_error = ""
 
@@ -28,7 +28,7 @@ def submit_credentials():
     if password == "":
         password_error = "That's not a valid password"
 
-    if len(password) < 5 or len(password) > 20:
+    if len(password) < 5 or len(password) > 20 or password.count(" ") != 0:
         password_error = "That's not a valid password"
 
     # Process password re-entry.
@@ -44,7 +44,11 @@ def submit_credentials():
     email_error = ""
 
     # Verify (optional) email address valid...
-
+    if email != "":
+        if len(email) < 3 or len(email) > 20 or email.count(" ") != 0 or email.count("@") != 1 or email.count(".") != 1:
+           email_error = "Invalid email address"
+        
+    # Flask magic
     return render_template('base.html', username=username, username_error=username_error, 
         password_error=password_error, password_verify_error=password_verify_error, email=email, email_error=email_error)
 
