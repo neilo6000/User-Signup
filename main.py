7c15f14c3a1edee1d0e30286f703d58242bd8df0
neilo6000 <neilo6000@gmail.com>
2018-07-12 10:41:14 -0600

    Version 3

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, url_for
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+# Declare global 'username' variable
+username = ""
+
 @app.route('/')
 def index():
+    app.secret_key = 'SDHFH(*#R&*(DGVFHNSDFJLHSDKHFJLDHJSF*(ASD^&F)DY*USHN'
     return render_template('base.html',title="User Signup")
 
 @app.route('/submit', methods=['POST'])
 def submit_credentials():
     # Process Username.
     username=request.form['username']
+    username = session['username'] 
     username_error = ""
 
     # Verify if Username is correct...
-    if username == "":
+    if username == "" or len(username)  < 3 or len(username) > 20:
         username_error = "That's not a valid username"
 
-    if len(username)  < 4 or len(username) > 20:
-        username_error = "That's not a valid username"
-        
     # Process Password.
     password=request.form['password'] 
     password_error = ""
 
     # Verify if password is correct...
-    if password == "":
-        password_error = "That's not a valid password"
-
-    if len(password) < 5 or len(password) > 20 or password.count(" ") != 0:
+    if password == "" or len(password) < 5 or len(password) > 20 or password.count(" ") != 0:
         password_error = "That's not a valid password"
 
     # Process password re-entry.
@@ -48,8 +47,16 @@ def submit_credentials():
         if len(email) < 3 or len(email) > 20 or email.count(" ") != 0 or email.count("@") != 1 or email.count(".") != 1:
            email_error = "Invalid email address"
         
-    # Flask magic
-    return render_template('base.html', username=username, username_error=username_error, 
-        password_error=password_error, password_verify_error=password_verify_error, email=email, email_error=email_error)
+    # Render some templates.
+    if not password_error and not username_error and not password_error and not password_verify_error and not email_error:
+        return redirect('/welcome')
+    else:
+        return render_template('base.html', username=username, username_error=username_error, 
+             password_error=password_error, password_verify_error=password_verify_error, email=email, 
+             email_error=email_error)
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html',username=username)
 
 app.run()
